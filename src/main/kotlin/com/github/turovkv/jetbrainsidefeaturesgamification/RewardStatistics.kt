40d806c313935cdde3a3752c36b6b959127b4579
kirill <turovkv@yandex.ru>
2021-04-23 02:13:49 +0300

    Subscribe to EventLogNotificationService, but its not working :(

--- src/main/kotlin/com/github/turovkv/jetbrainsidefeaturesgamification/RewardStatistics.kt
+++ src/main/kotlin/com/github/turovkv/jetbrainsidefeaturesgamification/RewardStatistics.kt
@@ -1,10 +1,14 @@
 package com.github.turovkv.jetbrainsidefeaturesgamification
 
 class RewardStatistics {
-    data class Reward(val name: String, val points: Int)
+    data class Reward(var name: String = "", var points: Int = 0)
 
     var accumulatedPoints: Int = 0
     var rewards = HashSet<Reward>()
+
+    companion object {
+        const val DEFAULT_REWARD: Int = 1
+    }
 
     fun addReward(name: String, points: Int) {
         accumulatedPoints += points
--- src/main/kotlin/com/github/turovkv/jetbrainsidefeaturesgamification/listeners/FeatureUsageListener.kt
+++ src/main/kotlin/com/github/turovkv/jetbrainsidefeaturesgamification/listeners/FeatureUsageListener.kt
@@ -1,20 +1,23 @@
 package com.github.turovkv.jetbrainsidefeaturesgamification.listeners
 
+import com.github.turovkv.jetbrainsidefeaturesgamification.RewardStatistics
 import com.github.turovkv.jetbrainsidefeaturesgamification.services.SavedStatistics
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.DataContext
-import com.intellij.openapi.actionSystem.ex.AnActionListener
+import com.intellij.ide.AppLifecycleListener
+import com.intellij.internal.statistic.eventLog.EventLogNotificationService
+import com.intellij.internal.statistic.eventLog.LogEvent
 import com.intellij.openapi.components.service
-import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.project.Project
 
-internal class FeatureUsageListener : AnActionListener {
-    override fun beforeActionPerformed(action: AnAction, dataContext: DataContext, event: AnActionEvent) {
-        val stats = service<SavedStatistics>().state
-        stats.addReward(action.toString(), 1)
-        if (stats.accumulatedPoints > 10) {
-            Messages.showMessageDialog(stats.rewards.toString(), "Just Saying", Messages.getInformationIcon())
-            stats.clear()
+internal class FeatureUsageListener : AppLifecycleListener {
+    override fun appStarting(projectFromCommandLine: Project?) {
+        println("Lets Go!")
+        EventLogNotificationService.subscribe({ println(it) }, "GameSys") //Not working :(
+        //EventLogNotificationService.subscribe(::subscriber , "GameSys")
+    }
+
+    fun subscriber(logEvent: LogEvent) {
+        if (logEvent.group.id == "productivity" && logEvent.event.id == "feature.used") {
+            service<SavedStatistics>().state.addReward(logEvent.toString(), RewardStatistics.DEFAULT_REWARD)
         }
     }
 }
