36d2b704c72fc62c8c24822b31224b49c2542e6f
Ekaterina Shelukhina <shelukchina2447@gmail.com>
2021-04-30 01:09:27 +0300

    add Button clear stat and Level

--- src/main/kotlin/com/intellij/plugin/gamification/actions/FeatureGamificationButton.kt
+++ src/main/kotlin/com/intellij/plugin/gamification/actions/FeatureGamificationButton.kt
@@ -8,10 +8,13 @@ import java.awt.EventQueue
 import java.lang.Exception
 import javax.swing.JFrame
 import javax.swing.JPanel
+import javax.swing.JButton
 import javax.swing.JProgressBar
 import javax.swing.SwingWorker
 import javax.swing.JLabel
 import javax.swing.border.EmptyBorder
+import java.awt.Color;
+import javax.swing.UIManager;
 
 
 class FeatureGamificationButton : AnAction() {
@@ -28,7 +31,7 @@ class FeatureGamificationButton : AnAction() {
 }
 
 class ProgressBar : JFrame() {
-    private val contentPane: JPanel
+    val contentPane: JPanel
 
     private class ProgressWorker(private val progress: JProgressBar) : SwingWorker<Void?, Int?>() {
         @Throws(Exception::class)
@@ -48,17 +51,34 @@ class ProgressBar : JFrame() {
 
     init {
         defaultCloseOperation = HIDE_ON_CLOSE
-        setBounds(0, 0, 200, 75)
+        setBounds(0, 0, 300, 125)
+
         contentPane = JPanel()
         contentPane.border = EmptyBorder(5, 5, 5, 5)
         contentPane.layout = BorderLayout(0, 0)
+
         val progress = JProgressBar()
+
+        val levelInfo = JLabel("Level: " + SavedStatistics.get().level.toString())
+
+        val clearButton = JButton("Clear Stats")
+        clearButton.addActionListener {
+            SavedStatistics.get().clear()
+            progress.value = SavedStatistics.get().getProgress()
+            levelInfo.setText("Level: " + SavedStatistics.get().level.toString())
+        }
+
         progress.isStringPainted = true
+
+
         contentPane.add(JLabel("Your progress: "), BorderLayout.NORTH)
-        contentPane.add(progress, BorderLayout.SOUTH)
+        contentPane.add(progress, BorderLayout.CENTER)
+        contentPane.add(clearButton, BorderLayout.SOUTH)
+        contentPane.add(levelInfo, BorderLayout.EAST)
         setContentPane(contentPane)
         val worker = ProgressWorker(progress)
         worker.execute()
+
     }
 }
 
