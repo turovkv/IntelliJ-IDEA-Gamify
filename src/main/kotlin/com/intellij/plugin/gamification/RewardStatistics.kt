d134e5a56674da83ef649fa87ede14f94520a4e1
kirill <turovkv@yandex.ru>
2021-05-03 00:25:33 +0300

    Add method for reward log

--- src/main/kotlin/com/intellij/plugin/gamification/RewardStatistics.kt
+++ src/main/kotlin/com/intellij/plugin/gamification/RewardStatistics.kt
@@ -1,32 +1,60 @@
 package com.intellij.plugin.gamification
 
+import com.intellij.featureStatistics.ProductivityFeaturesRegistry
 import com.intellij.internal.statistic.eventLog.LogEvent
 
 class RewardStatistics {
+    var points: Int = 0
     var level: Int = 0
-    var pointsInLevel: Int = 400
-    var explorationPoints: Int = 0
-    var countExplorationUsages = HashMap<String, Int>()
+
+    var countFeatureUsages: MutableMap<String, Int> = HashMap()
+    var pointsPerFeature: MutableMap<String, Int> = HashMap()
+
+    val pointsInLevel: Int = 400
 
     fun addEvent(logEvent: LogEvent) {
         val name = logEvent.event.data["id"].toString()
-        countExplorationUsages[name] = countExplorationUsages.getOrDefault(name, 0) + 1
-        explorationPoints += when (countExplorationUsages[name]) {
+        val oldCount = countFeatureUsages.getOrDefault(name, 0)
+        val oldPoints = pointsPerFeature.getOrDefault(name, 0)
+        val newPoints = getPointsForEvent(name)
+
+        points += newPoints
+        level = points / pointsInLevel
+
+        countFeatureUsages[name] = oldCount + 1
+        pointsPerFeature[name] = oldPoints + newPoints
+    }
+
+    private fun getPointsForEvent(name: String): Int {
+        return when (countFeatureUsages[name]) {
             1 -> 100
             2 -> 60
             3 -> 30
             4 -> 10
             else -> 0
         }
-        level = explorationPoints / pointsInLevel
-        println("level = " + level + " progress = " + getProgress() + " points = " + explorationPoints)
     }
 
-    fun getProgress() = (100 * (explorationPoints % pointsInLevel)) / pointsInLevel
+    fun getRewardList(): List<List<String>> {
+        return pointsPerFeature.map {
+            val dname = getDisplayName(it.key)
+            if (dname != null) {
+                listOf(dname, it.value.toString())
+            } else {
+                null
+            }
+        }.filterNotNull()
+    }
+
+    private fun getDisplayName(name: String) =
+        ProductivityFeaturesRegistry.getInstance()?.getFeatureDescriptor(name)?.displayName
+
+    fun getProgress() = (100 * (points % pointsInLevel)) / pointsInLevel
 
     fun clear() {
         level = 0
-        explorationPoints = 0
-        countExplorationUsages = HashMap()
+        points = 0
+        countFeatureUsages = HashMap()
+        pointsPerFeature = HashMap()
     }
 }
