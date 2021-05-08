565cfe8a77063bc11b2794f70247b79d6a7c7e81
kirill <turovkv@yandex.ru>
2021-05-08 13:21:47 +0300

    Mark FeatureGamificationButton with interface DumbAware

--- src/main/kotlin/com/intellij/plugin/gamification/actions/FeatureGamificationButton.kt
+++ src/main/kotlin/com/intellij/plugin/gamification/actions/FeatureGamificationButton.kt
@@ -2,9 +2,10 @@ package com.intellij.plugin.gamification.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAware
 import java.awt.EventQueue
 
-class FeatureGamificationButton : AnAction() {
+class FeatureGamificationButton : AnAction(), DumbAware {
     override fun actionPerformed(event: AnActionEvent) {
         ShowGameStatisticsDialog(getEventProject(event)).show()
     }
