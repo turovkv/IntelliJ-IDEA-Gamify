1d3938b3edd4f344af47f44db3c875c3867aa449
Ekaterina Shelukhina <shelukchina2447@gmail.com>
2021-05-20 20:46:28 +0300

    Add to ToolWindow

--- src/main/kotlin/com/intellij/plugin/gamification/widget/GameProgressWidgetFactory.kt
+++ src/main/kotlin/com/intellij/plugin/gamification/widget/GameProgressWidgetFactory.kt
@@ -2,11 +2,10 @@ package com.intellij.plugin.gamification.widget
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.wm.StatusBar
-import com.intellij.openapi.wm.StatusBarWidget
-import com.intellij.openapi.wm.StatusBarWidgetFactory
+import com.intellij.openapi.wm.*
 import com.intellij.plugin.gamification.actions.GameStatisticsDialog
 import com.intellij.ui.ClickListener
+import com.intellij.ui.content.ContentFactory
 import java.awt.event.MouseEvent
 
 class GameProgressWidgetFactory : StatusBarWidgetFactory {
@@ -30,7 +29,24 @@ class GameProgressWidgetFactory : StatusBarWidgetFactory {
         val panel = GameProgressPanel()
         object : ClickListener() {
             override fun onClick(event: MouseEvent, clickCount: Int): Boolean {
-                GameStatisticsDialog(project).show()
+//                GameStatisticsDialog(project).show()
+                val toolWindowManager = ToolWindowManager.getInstance(project)
+
+                var toolWindow = toolWindowManager.getToolWindow("My Progress")
+                if (toolWindow == null) {
+                    toolWindow = toolWindowManager.registerToolWindow("My Progress", true, ToolWindowAnchor.BOTTOM)
+
+                    val contentManager = toolWindow.contentManager
+                    val contentFactory = ContentFactory.SERVICE.getInstance()
+                    contentManager.addContent(
+                        contentFactory.createContent(
+                            GameStatisticsDialog(project).contentPanel,
+                            "",
+                            false
+                        )
+                    )
+                }
+                toolWindow.activate(null)
                 return true
             }
         }.installOn(panel, true)
