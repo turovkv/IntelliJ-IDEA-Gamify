740a90e7ad18ffb685d5ad6b0cd5a902c791d256
kirill <turovkv@yandex.ru>
2021-06-03 20:36:20 +0300

    Change val to var in User fields

--- src/main/kotlin/com/intellij/plugin/gamification/services/network/User.kt
+++ src/main/kotlin/com/intellij/plugin/gamification/services/network/User.kt
@@ -1,12 +1,12 @@
 package com.intellij.plugin.gamification.services.network
 
 data class User(
-    val name: String,
+    var name: String,
     var userInfo: UserInfo = UserInfo()
 )
 
 data class UserInfo(
-    val displayName: String = "No display name",
-    val level: Int = 1,
-    val progress: Int = 0,
+    var displayName: String = "No display name",
+    var level: Int = 1,
+    var progress: Int = 0,
 )
